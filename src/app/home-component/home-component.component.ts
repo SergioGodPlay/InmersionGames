e6243971c1e89e6d756e086a23f698a599ffa82c
Sergio Sanchez <richardgodplay@gmail.com>
2022-05-02 19:34:50 -0500

    Agregada funcionalidad al slider

--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -1,20 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, VERSION } from '@angular/core';
+
 
 @Component({
   selector: 'app-home-component',
   templateUrl: './home-component.component.html',
-  styleUrls: ['./home-component.component.css']
+  styleUrls: ['./home-component.component.css'],
 })
 
 export class HomeComponentComponent implements OnInit {
 
+  contador:number = 0;
+
   constructor() { 
+
+    
     
   }
 
   ngOnInit(): void {
 
- 
+    //setInterval(this.moveRight, 3000);
+
+  }
+
+
+  
+
+  moveRight(){
+
+    const slider = document.querySelector("#slider");
+
+    let sliderSection = document.querySelectorAll(".slider__section");
+
+    //let sliderSectionLast = sliderSection[sliderSection.length - 1];
+
+    let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+
+    slider?.insertAdjacentElement("beforeend", sliderSectionFirst);
+
+  }
+
+  moveLeft(){
+
+    const slider = document.querySelector("#slider");
+
+    let sliderSection = document.querySelectorAll(".slider__section");
+
+    let sliderSectionLast = sliderSection[sliderSection.length - 1];
+
+    //let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+
+    slider?.insertAdjacentElement("afterbegin", sliderSectionLast);
+
   }
 
 }
